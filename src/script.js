6a4ac65221f233a4ce592d36039b5a75ac49ee80
Ram Saini <sainiram226@gmail.com>
2025-09-26 08:14:08 +0530

    Portfolio

---
+++ src/script.js
@@ -0,0 +1,19 @@
+// Initialize Lenis
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
+    smooth: true,
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('open');
+}
